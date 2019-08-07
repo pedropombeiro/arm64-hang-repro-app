acd05a3e40666201e3cda5f8b6922e06750b4a90
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-08-07 10:59:28 +0200

    Upgrade to RN 0.60.4 (Hermes disabled)

--- android/app/src/main/java/com/arm64test/MainApplication.java
+++ android/app/src/main/java/com/arm64test/MainApplication.java
@@ -1,17 +1,19 @@
 package com.arm64test;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import im.status.ethereum.module.StatusPackage;
-
-import java.util.Arrays;
 import java.util.List;
+
+import im.status.ethereum.module.StatusPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -23,10 +25,12 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new StatusPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      packages.add(new StatusPackage());
+      return packages;
     }
 
     @Override
